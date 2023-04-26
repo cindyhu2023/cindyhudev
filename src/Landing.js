44474722bc269d038ad58aa7f66fa7f0725c4699
cindyhu2023 <cindyhu2023@u.northwestern.edu>
2023-04-26 15:37:51 -0500

    add landing

---
+++ src/Landing.js
@@ -0,0 +1,30 @@
+import './App.css';
+import './Landing.css';
+import TypeWriterEffect from 'react-typewriter-effect';
+
+export function Landing() {
+  return (
+    <div className="Landing">
+    <div className='LandingText'>
+        <TypeWriterEffect
+                textStyle={{ textAlign: 'center',}}
+                startDelay={500}
+                cursorColor="#3F3D56"
+                multiText={[
+                'Hello!',
+                '哈囉！',
+                'Hey there!',
+                '你好！',
+                'Welcome!',
+                '歡迎！'
+                ]}
+                multiTextDelay={2000}
+                typeSpeed={100}
+                multiTextLoop={true}
+            />
+            <h1>I'm Cindy Hu</h1>
+            <h2>Passionate about human-computer interaction and technology that makes everyday life a little more enjoyable</h2>
+        </div>
+    </div>
+  );
+}
